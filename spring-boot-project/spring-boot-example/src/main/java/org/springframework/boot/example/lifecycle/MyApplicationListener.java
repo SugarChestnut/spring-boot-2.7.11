7b79495caceca3363b8ffa25933efc5146abf529
rtt <1191623980@qq.com>
2024-06-25 23:58:05 +0800

    0625

--- spring-boot-project/spring-boot-example/src/main/java/org/springframework/boot/example/lifecycle/MyApplicationListener.java
+++ spring-boot-project/spring-boot-example/src/main/java/org/springframework/boot/example/lifecycle/MyApplicationListener.java
@@ -2,11 +2,13 @@ package org.springframework.boot.example.lifecycle;
 
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
 /**
  * @author rtt
  * @date 2023/5/4 13:33
  */
+@Component
 public class MyApplicationListener implements ApplicationListener<ApplicationEvent> {
 
     public MyApplicationListener() {
@@ -15,6 +17,6 @@ public class MyApplicationListener implements ApplicationListener<ApplicationEve
 
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
-        System.out.println(event.toString());
+        System.out.println("[MyApplicationListener]" + event.getClass());
     }
 }
